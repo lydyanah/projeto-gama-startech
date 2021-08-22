87166760fde3b8bd12cef75cd61cbfc677aa6c22
Lidi <lidiana.tdsc@gmail.com>
2021-08-22 09:08:21 -0300

    container

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { Field, Form, Formik } from "formik";
 import { Select, TextField } from "formik-material-ui";
-import { Button, Box, Paper } from "@material-ui/core";
+import { Button, Box, Paper, Container } from "@material-ui/core";
 import { KeyboardDatePicker } from "formik-material-ui-pickers";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
@@ -35,206 +35,210 @@ const Home = () => {
               // validationSchema={}
             >
               {({ submitForm, isSubmitting }) => (
-               
-                <MuiPickersUtilsProvider utils={DateFnsUtils} locale={brLocale}>
-                  <Form className={classes.form}>
-                    <h1>Dados Pessoais</h1>
-                    <Paper className={classes.root}>
+                <Container maxWidth="md">
+                  <MuiPickersUtilsProvider
+                    utils={DateFnsUtils}
+                    locale={brLocale}
+                  >
+                    <Form className={classes.form}>
+                      <h1>Dados Pessoais</h1>
+                      <Paper className={classes.root}>
+                        <Box>
+                          <h2>Nome Completo</h2>
+                          <Field
+                            className={classes.field}
+                            name="nome"
+                            component={TextField}
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Cargo Pretendido</h2>
+                          <Field
+                            className={classes.field}
+                            name="cargo"
+                            component={TextField}
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Data de Nascimento</h2>
+                          <Field
+                            className={classes.field}
+                            component={KeyboardDatePicker}
+                            name="dataNascimento"
+                            format="dd/MM/yy"
+                            disableFuture="true"
+                            inputVariant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Estado Civil</h2>
+                          <Field
+                            className={classes.field}
+                            component={Select}
+                            name="estadoCivil"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Gênero</h2>
+                          <Field
+                            className={classes.field}
+                            component={Select}
+                            name="genero"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Endereço</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="endereço"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Bairro</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="bairro"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Cidade</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="cidade"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>CEP</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="cep"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Telefone Fixo 1</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="telefone"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Telefone Fixo 2</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="telefone"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Celular</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="celular"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Contato</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="contato"
+                            type="text"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>E-mail</h2>
+                          <Field
+                            className={classes.field}
+                            component={TextField}
+                            name="email"
+                            type="email"
+                            variant="outlined"
+                          />
+                        </Box>
+                      </Paper>
+                      <h1>Documentos</h1>
+                      <Paper className={classes.root}>
+                        <Box>
+                          <h2>Identidade</h2>
+                          <Field
+                            className={classes.field}
+                            name="identidade"
+                            component={TextField}
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>CPF</h2>
+                          <Field
+                            className={classes.field}
+                            name="cpf"
+                            component={TextField}
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Possui veículo?</h2>
+                          <Field
+                            className={classes.field}
+                            component={Select}
+                            name="veiculo"
+                            variant="outlined"
+                          />
+                        </Box>
+                        <Box>
+                          <h2>Habilitação</h2>
+                          <Field
+                            className={classes.field}
+                            component={Select}
+                            name="habilitação"
+                            variant="outlined"
+                          />
+                        </Box>
+                      </Paper>
                       <Box>
-                        <h2>Nome Completo</h2>
-                        <Field
-                          className={classes.field}
-                          name="nome"
-                          component={TextField}
-                          variant="outlined"
-                        />
+                        <Button
+                          classes={{
+                            root: classes.submitButton,
+                            label: classes.label,
+                          }}
+                          color="primary"
+                          disabled={isSubmitting}
+                          disableElevation
+                          onClick={submitForm}
+                          variant="contained"
+                        >
+                          Enviar
+                        </Button>
                       </Box>
-                      <Box>
-                        <h2>Cargo Pretendido</h2>
-                        <Field
-                          className={classes.field}
-                          name="cargo"
-                          component={TextField}
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Data de Nascimento</h2>
-                        <Field
-                          className={classes.field}
-                          component={KeyboardDatePicker}
-                          name="dataNascimento"
-                          format="dd/MM/yy"
-                          disableFuture="true"
-                          inputVariant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Estado Civil</h2>
-                        <Field
-                          className={classes.field}
-                          component={Select}
-                          name="estadoCivil"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Gênero</h2>
-                        <Field
-                          className={classes.field}
-                          component={Select}
-                          name="genero"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Endereço</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="endereço"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Bairro</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="bairro"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Cidade</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="cidade"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>CEP</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="cep"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Telefone Fixo 1</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="telefone"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Telefone Fixo 2</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="telefone"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Celular</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="celular"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Contato</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="contato"
-                          type="text"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>E-mail</h2>
-                        <Field
-                          className={classes.field}
-                          component={TextField}
-                          name="email"
-                          type="email"
-                          variant="outlined"
-                        />
-                      </Box>
-                    </Paper>
-                    <h1>Documentos</h1>
-                    <Paper className={classes.root}>
-                      <Box>
-                        <h2>Identidade</h2>
-                        <Field
-                          className={classes.field}
-                          name="identidade"
-                          component={TextField}
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>CPF</h2>
-                        <Field
-                          className={classes.field}
-                          name="cpf"
-                          component={TextField}
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Possui veículo?</h2>
-                        <Field
-                          className={classes.field}
-                          component={Select}
-                          name="veiculo"
-                          variant="outlined"
-                        />
-                      </Box>
-                      <Box>
-                        <h2>Habilitação</h2>
-                        <Field
-                          className={classes.field}
-                          component={Select}
-                          name="habilitação"
-                          variant="outlined"
-                        />
-                      </Box>
-                    </Paper>
-                    <Box>
-                      <Button
-                        classes={{
-                          root: classes.submitButton,
-                          label: classes.label,
-                        }}
-                        color="primary"
-                        disabled={isSubmitting}
-                        disableElevation
-                        onClick={submitForm}
-                        variant="contained"
-                      >
-                        Enviar
-                      </Button>
-                    </Box>
-                    {isSubmitting}
-                  </Form>
-                </MuiPickersUtilsProvider>
+                      {isSubmitting}
+                    </Form>
+                  </MuiPickersUtilsProvider>
+                </Container>
               )}
             </Formik>
           </section>
